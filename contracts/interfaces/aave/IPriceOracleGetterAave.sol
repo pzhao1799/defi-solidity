967d2c00515e8040c060ba7e6b7663e888c2d4b7
healthyred <jhl3@williams.edu>
2022-01-08 12:46:48 -0500

    added aave actions

---
+++ contracts/interfaces/aave/IPriceOracleGetterAave.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier: agpl-3.0
+
+pragma solidity =0.7.6;
+
+abstract contract IPriceOracleGetterAave {
+    function getAssetPrice(address _asset) external virtual view returns (uint256);
+    function getAssetsPrices(address[] calldata _assets) external virtual view returns(uint256[] memory);
+    function getSourceOfAsset(address _asset) external virtual view returns(address);
+    function getFallbackOracle() external virtual view returns(address);
+}
