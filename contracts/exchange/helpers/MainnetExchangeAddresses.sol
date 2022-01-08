2f226065be57a8a6f5b3b439a6065defbfebe8cd
healthyred <jhl3@williams.edu>
2022-01-08 12:32:56 -0500

    removed fees

--- contracts/exchange/helpers/MainnetExchangeAddresses.sol
+++ contracts/exchange/helpers/MainnetExchangeAddresses.sol
@@ -5,8 +5,8 @@ pragma solidity =0.7.6;
 contract MainnetExchangeAddresses {
 
     // TODO: remove fee_recipients as we are never gonna use it
-    address internal constant FEE_RECIPIENT_ADDRESS = 0x39C4a92Dc506300c3Ea4c67ca4CA611102ee6F2A;
-    address internal constant DISCOUNT_ADDRESS = 0x1b14E8D511c9A4395425314f849bD737BAF8208F;
+    // address internal constant FEE_RECIPIENT_ADDRESS = 0x000000000000000000000000000000000000dEaD;
+    // address internal constant DISCOUNT_ADDRESS = 0x1b14E8D511c9A4395425314f849bD737BAF8208F;
     address internal constant PROTECTOR_EXCHANGE_REGISTRY = 0x25dd3F51e0C3c3Ff164DDC02A8E4D65Bb9cBB12D;
     address internal constant AVAX_BURN_ADDRESS = 0x000000000000000000000000000000000000dEaD;
 }
